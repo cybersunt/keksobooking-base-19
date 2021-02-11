6658634098f656605eb25d5c1e3646c2c186244b
Maksimova, Yuliya <Yuliya.Maksimova@nexign.com>
2021-02-11 16:39:52 +0300

    Добавлен модуль форм

---
+++ js/form.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(function () {
+  var formAd = document.querySelector('.ad-form');
+  var numberOfRooms = formAd.querySelector('#room_number');
+  var numberOfSeats = formAd.querySelector('#capacity');
+
+  numberOfRooms.addEventListener('change', function (evt) {
+
+
+    if (evt.target.value == 1) {
+
+    }
+    if (evt.target.value == 2) {
+
+    }
+    if (evt.target.value == 3) {
+
+    }
+    if (evt.target.value == 100) {
+
+    }
+  })
+
+})();
